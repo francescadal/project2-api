e4e21c341e7cb466ca64253ef9ffafa424a7dc82
francescadal <francescadalembert@icloud.com>
2015-11-13 09:30:45 -0500

    Erased

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   post '/register' => 'auth#register'
   # patch '/confirm' => 'auth#confirm'
   post '/login' => 'auth#login'
+
   delete '/logout/:id' => 'auth#logout'
 
   resources :users, except: [:new, :edit]
