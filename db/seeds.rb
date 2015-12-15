c4478bc9d03cc1ba8f47c2109ed206963c8793f6
francescadal <francescadalembert@icloud.com>
2015-12-15 10:20:29 -0500

    trying to get comment delte to function

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,3 @@
 Comment.delete_all
 User.delete_all
 
-# Create 3 users
-%w(tom fran dan bill).each do |name|
-  email = "#{name}@example.com"
-  User.create!(email: email, password: 'pw',
-               password_confirmation: 'pw')
-end
-
-tom = User.find_by_email('tom@example.com')
-tom.comments.create!(content: "hello world")
-
-fran = User.find_by_email('fran@example.com')
-fran.comments.create!(content: "nice things")
-
-dan = User.find_by_email('dan@example.com')
-dan.comments.create!(content: "nice things")
